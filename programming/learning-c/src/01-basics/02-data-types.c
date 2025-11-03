0b5e8cf392f92264fa22e5557ca745b19c6650bd
Pablo Arturo <pabloarturojmzdlc2004@gmail.com>
2025-11-03 16:33:00 -0600

    add notes about basics data types in C

---
+++ programming/learning-c/src/01-basics/02-data-types.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    // Declare variables
+    int age = 25;        // Integer number
+    float height = 1.75; // Decimal number
+    char initial = 'A';  // Single character
+    char name[] = "Ana"; // Text string
+
+    // Print variables
+    printf("Name: %s\n", name);
+    printf("Initial: %c\n", initial);
+    printf("Age: %d\n", age);
+    printf("Height: %f\n", height);
+
+    return 0;
+}
